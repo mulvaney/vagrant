ca43ac4e2c244ff2b06735fdc0ceb9d8817dc2a5
Mike Mulvaney <vagrant@precise32.(none)>
2013-06-17 18:46:21 +0000

    updated some cookbooks

--- setup.rb
+++ setup.rb
@@ -5,17 +5,9 @@ topdir = File.absolute_path(File.dirname(__FILE__))
 cookbooks_dir = File.join(topdir, 'cookbooks')
 
 cookbooks = {}
-["apt",
- "git",
- "java",
- "memcached",
- "runit",
- "build-essential",
- "yum",
- "windows",
- "chef_handler",
- "ohai"].each do |repo|
-  cookbooks[repo] = "https://github.com/opscode-cookbooks/#{repo}.git"
+["apt", "git", "java", "memcached", "runit", "build-essential", "yum", "windows",
+  "chef_handler", "ohai"].each do |repo|
+    cookbooks[repo] = "https://github.com/opscode-cookbooks/#{repo}.git"
 end
 
 cookbooks["rbenv"] = "https://github.com/RiotGames/rbenv-cookbook.git rbenv",
